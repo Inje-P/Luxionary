51dbbda66084db417e3066f01528a1eb14d5a729
Inje Pak <grandefidelite@gmaill.com>
2022-01-24 11:21:15 +0900

    TabLayout 추가 (1/24/2022)

--- app/src/main/java/com/luan/luxionary/StartActivity.java
+++ app/src/main/java/com/luan/luxionary/StartActivity.java
@@ -6,14 +6,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import java.util.ArrayList;
 
 public class StartActivity extends AppCompatActivity {
 
     ViewPager2 viewPager2;
+    TabLayout tabLayout;
     Button btnGoogle, btnFacebook, btnEmail;
 
     @Override
@@ -22,6 +27,7 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
 
         viewPager2 = findViewById(R.id.viewPager2);
+        tabLayout = findViewById(R.id.tabLayout);
         btnGoogle = findViewById(R.id.btnGoogle);
         btnFacebook = findViewById(R.id.btnFacebook);
         btnEmail = findViewById(R.id.btnEmail);
@@ -36,6 +42,14 @@ public class StartActivity extends AppCompatActivity {
         list.add(new DataPage(R.drawable.viewpager3, "럭셔리한 나의 작은 습관", "평범한 일상에 품격을 더해보세요"));
 
         viewPager2.setAdapter(new ViewPagerAdapter(list));
+
+        new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+
+            }
+        }).attach();
+
     }
 
     View.OnClickListener mClickListener = new View.OnClickListener() {
